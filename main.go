35ae16b34ce25f74ffe8f1510223aa942be42c6b
Renespeare <mridhor24@gmail.com>
2023-04-14 22:37:32 +0700

    added port for railway

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"final-project-dts-go/database"
 	"final-project-dts-go/router"
+	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 func main()  {
-	var PORT = ":8080"
+	// var PORT = ":8080"
 	database.StartDB()
 
 	fmt.Println("Successfully connected to database")
 
-	routers.StartServer().Run(PORT)
+	// routers.StartServer().Run(PORT)
+	routers.StartServer().Run(os.Getenv("PORT"))
 }
